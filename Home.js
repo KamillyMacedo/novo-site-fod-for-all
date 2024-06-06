f01754718f43ce1093255aa75f1c71a646a35983
Felipenfe <160440848+Felipenfe@users.noreply.github.com>
2024-06-06 19:08:42 -0300

    Update and rename Home-login.js to Home.js

--- Home.js
+++ Home.js
@@ -10,3 +10,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
         window.location.href = 'login.html'; // Substitua 'login.html' pelo caminho da sua página de login
     });
 });
+
+document.getElementById('fazer-parte-button').addEventListener('click', function() {
+    window.location.href = 'Como contribuir.html';
+});
+
+
+document.getElementById('ser-beneficiado').addEventListener('click', function() {
+    window.location.href = 'Ser um beneficiário.html';
+});
